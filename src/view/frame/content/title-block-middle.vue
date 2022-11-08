828f46920754b7cfe3b4bf69490bceca6de273e1
杨晓强 <xiaoqiang2.yang@zhaopin.com.cn>
2022-11-08 16:11:32 +0800

    标签页的完成

--- src/view/frame/content/title-block-middle.vue
+++ src/view/frame/content/title-block-middle.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="scroll-outer" ref="outerDivRef" @mousedown="outerDivMouseDown">
     <div class="scroll-body">
-      <template v-for="index in 200" :key="index">
-        <a-tag :size="'large'" style="margin-left: 10px" :bordered="true" closable> Tag </a-tag>
+      <a-tag :id="'/'"
+             :color="'/' == route.path?'green':'gray'"
+             size="large"
+             style="margin-left: 10px"
+             :bordered="true"
+             @click="clickTag('/')">
+        <template #icon>
+          <icon-home/>
+        </template>
+        首页
+      </a-tag>
+      <template v-for="(tag,index) in tagOptions">
+        <a-tag :id="tag.path"
+               :color="tag.path === route.path?'green':'gray'"
+               size="large"
+               style="margin-left: 10px"
+               :bordered="true"
+               @click="clickTag(tag.path)">
+          {{ tag.name }}
+          <icon-close class="tagCloseIcon" @click.stop="closeTag(index)"/>
+        </a-tag>
       </template>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {onMounted, onUnmounted, ref} from "vue";
+import {nextTick, onMounted, onUnmounted, ref, watch} from "vue";
 import * as $L from 'owner-tool-js';
+import {useRoute, useRouter} from "vue-router";
 
 const outerDivRef = ref();
+// 用户获取当前路由的数据
+const route = useRoute();
+const router = useRouter();
+// 标签选项显示
+const tagOptions = ref([]);
+
 
 // outerDiv被鼠标点下去的时候的滚动轴位置
 let outerDivScrollMouseDownPositionX: any = 0;
@@ -36,6 +62,60 @@ const outerDivMouseDown = (event: any) => {
   watcher.open();
 }
 
+/**
+ * 点击标签要进行跳转
+ * @param
+ * @return
+ * @author     :loulan
+ * */
+const clickTag = (path: string) => {
+  router.push(path);
+}
+
+/**
+ * 关闭标签页的时候
+ * @param
+ * @return
+ * @author     :loulan
+ * */
+const closeTag = (index: number) => {
+  const tagData: any = tagOptions.value[index];
+  // 判断关闭是否是当前路由的标签，如果是需要向前一个，否则直接关闭就可以
+  if (tagData.path === route.path) {
+    if (index > 0) {
+      router.push(tagOptions.value[index - 1]);
+    } else {
+      // 判断是否就剩下一个了，如果是，那么路由到首页，否则路由到后面一个
+      router.push(tagOptions.value.length > 1 ? tagOptions.value[index + 1] : '/')
+    }
+  }
+  // 从标签选项中删除指定索引的标签数据
+  tagOptions.value.splice(index, 1);
+}
+
+watch(() => route.path,
+    (val: any) => {
+      // 先判断当前的路由是否已经在标签表中存在，如果存在就不需要再进行添加了（首页是肯定存在的，所以如果路由的首页是不需要添加标签的）
+      if (!(val == '/' || tagOptions.value.some((o: any) => o.path === val))) {
+        const temp = {
+          name: route.name,
+          path: val
+        };
+        tagOptions.value.unshift(temp);
+      }
+
+      nextTick(() => {
+        // 这里是通过租价id获取组件的位置然后操作滚动条进行显示的，所以必须要渲染完毕之后才能进行执行
+        // fixme 如果可以的话调整为只要在窗口内不进行滚动条的操作，后面来的到最后面，前面来的到最前面。
+        const dom: any = document.getElementById(val);
+        $L.windowsTool.setScrollPosition(dom.offsetLeft, 0, outerDivRef.value)
+      })
+    },
+    {
+      deep: true,
+      immediate: true
+    }
+)
 
 onMounted(() => {
   watcher = $L.windowsTool.watchMousePosition((x: number, y: number) => {
@@ -88,5 +168,16 @@ onUnmounted(() => {
   white-space: nowrap;
   overflow: visible;
   /*border: 1px solid red;*/
+}
+
+/*标签关闭图标样式*/
+.tagCloseIcon {
+  cursor: pointer;
+  margin-left: 5px;
+}
+
+.tagCloseIcon:hover {
+  color: blue;
+  font-weight: bolder;
 }
 </style>
