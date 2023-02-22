7cad2eab17d14af4d86c5413c3a3a8f1a633e24a
loulan <loulangogogo@163.com>
2023-02-22 19:43:25 +0800

    bug修改

--- src/view/frame/header/user-center/wechat.vue
+++ src/view/frame/header/user-center/wechat.vue
@@ -1,6 +1,6 @@
 <template>
   <div align="center" style="height: 100%">
-    <icon-wechat style="width: 50%;height: 50%"/>
+    <icon-wechat style="width: 50%;height: 50%;max-width: 150px;min-width: 50px"/>
     <span class="spanfont">绑定微信</span>
   </div>
 </template>
