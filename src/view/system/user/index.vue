3c52ba1118d83400a0df4370d2147310dd16668e
loulang <loulangogogo@163.com>
2023-12-14 17:16:06 +0800

    功能测试

--- src/view/system/user/index.vue
+++ src/view/system/user/index.vue
@@ -1,49 +1,4 @@
-<template>
-  <div class="headerDiv">
-    <a-input v-model="queryParam.username" style="width: 200px" placeholder="请输入用户名" allow-clear/>
-    <a-input v-model="queryParam.name" style="width: 200px;margin-left: 20px" placeholder="请输入姓名" allow-clear/>
-    <a-input v-model="queryParam.phone" style="width: 200px;margin-left: 20px" placeholder="请输入手机号码"
-             allow-clear/>
-    <a-button type="primary" style="margin-left: 20px" @click="search">查询</a-button>
-    <a-button v-if="!props.isRole" type="primary" status="success" style="margin-left: 20px" @click="add">添加</a-button>
-  </div>
-  <div class="bodyDiv">
-    <a-table :columns="props.isRole?roleColumns:columns"
-             :data="tableData"
-             :stripe="true"
-             page-position="bottom"
-             :pagination="{
-                total: queryParam.pageTotal,
-                showTotal: true,
-                showJumper: true,
-                showPageSize: true,
-                pageSizeOptions:[10,20,30,40,50],
-                current:queryParam.pageCurrent,
-                pageSize: queryParam.pageSize,
-              }"
-             :scroll="{
-                y: '100%'
-              }"
-             :scrollbar="false"
-             column-resizable
-             :bordered="{cell:true}"
-             :loading="loading"
-             @page-size-change="pageSizeChange"
-             @page-change="pageChange">
-      <template #operate="{record}">
-        <a-button type="primary" size="mini" @click="edit(record)">编辑</a-button>
-        <a-button type="primary" status="danger" size="mini" style="margin-left: 10px" @click="del(record)">删除
-        </a-button>
-      </template>
-    </a-table>
-  </div>
-  <div v-show="false">
-    <info ref="infoRef" @query="search"></info>
-  </div>
-</template>
-
 <script lang="ts" setup>
-
 import Info from './info.vue';
 import {onMounted, reactive, ref} from "vue";
 import {pageUserList, userDel} from "../../../common/api/system/user";
@@ -53,12 +8,13 @@ import {dragonConfirm, DragonNotice} from "../../../common/domain/component";
 import {core as coreTool} from "owner-tool-js";
 import {SpecialValueEnum} from "../../../common/domain/enums";
 
+
 const props = withDefaults(defineProps<{
-  // 是否是通过角色查询
+  // 是否是通过角色查询(角色查询是在角色管理中查询用户)
   isRole: boolean;
   // 高度设置
   contentHeight: number;
-}>(),{
+}>(), {
   isRole: false,
   seconds: 0
 })
@@ -99,10 +55,10 @@ const columns: Array<TableColumnData> = [
     title: "部门",
     dataIndex: "deptName",
     width: 150,
-    render: (data:{ record: TableData, column: TableColumnData, rowIndex: number}) => {
+    render: (data: { record: TableData, column: TableColumnData, rowIndex: number }) => {
       if (coreTool.isNotExist(data.record?.deptId)) {
         return "无";
-      }else if (data.record.deptId == SpecialValueEnum.TOP) {
+      } else if (data.record.deptId == SpecialValueEnum.TOP) {
         return "顶级";
       } else {
         return data.record.deptName;
@@ -162,10 +118,10 @@ const roleColumns: Array<TableColumnData> = [
     title: "部门",
     dataIndex: "deptName",
     width: 150,
-    render: (data:{ record: TableData, column: TableColumnData, rowIndex: number}) => {
+    render: (data: { record: TableData, column: TableColumnData, rowIndex: number }) => {
       if (coreTool.isNotExist(data.record?.deptId)) {
         return "无";
-      }else if (data.record.deptId == SpecialValueEnum.TOP) {
+      } else if (data.record.deptId == SpecialValueEnum.TOP) {
         return "顶级";
       } else {
         return data.record.deptName;
@@ -300,9 +256,9 @@ onMounted(() => {
 defineExpose({
   /**
    * 根据角色查询
-   * @param       
+   * @param
    * @return
-   * @exception  
+   * @exception
    * @author     :loulan
    * */
   searchByRole: (roleId: any) => {
@@ -313,6 +269,51 @@ defineExpose({
   }
 });
 </script>
+
+<template>
+  <div class="headerDiv">
+    <a-input v-model="queryParam.username" style="width: 200px" placeholder="请输入用户名" allow-clear/>
+    <a-input v-model="queryParam.name" style="width: 200px;margin-left: 20px" placeholder="请输入姓名" allow-clear/>
+    <a-input v-model="queryParam.phone" style="width: 200px;margin-left: 20px" placeholder="请输入手机号码"
+             allow-clear/>
+    <a-button type="primary" style="margin-left: 20px" @click="search">查询</a-button>
+    <a-button v-if="!props.isRole" type="primary" status="success" style="margin-left: 20px" @click="add">添加
+    </a-button>
+  </div>
+  <div class="bodyDiv">
+    <a-table :columns="props.isRole?roleColumns:columns"
+             :data="tableData"
+             :stripe="true"
+             page-position="bottom"
+             :pagination="{
+                total: queryParam.pageTotal,
+                showTotal: true,
+                showJumper: true,
+                showPageSize: true,
+                pageSizeOptions:[10,20,30,40,50],
+                current:queryParam.pageCurrent,
+                pageSize: queryParam.pageSize,
+              }"
+             :scroll="{
+                y: '100%'
+              }"
+             :scrollbar="false"
+             column-resizable
+             :bordered="{cell:true}"
+             :loading="loading"
+             @page-size-change="pageSizeChange"
+             @page-change="pageChange">
+      <template #operate="{record}">
+        <a-button type="primary" size="mini" @click="edit(record)">编辑</a-button>
+        <a-button type="primary" status="danger" size="mini" style="margin-left: 10px" @click="del(record)">删除
+        </a-button>
+      </template>
+    </a-table>
+  </div>
+  <div v-show="false">
+    <info ref="infoRef" @query="search"></info>
+  </div>
+</template>
 
 <style scoped>
 .headerDiv {
