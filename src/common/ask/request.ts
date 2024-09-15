91e3b1cca56416aa9edb54f01c3197b66e7cb00b
loulan <loulangogogo@163.com>
2024-09-15 20:23:30 +0800

    bug的修改和调整

--- src/common/ask/request.ts
+++ src/common/ask/request.ts
@@ -1,4 +1,4 @@
-import {AxiosRequestConfig, InternalAxiosRequestConfig} from "axios";
+import {InternalAxiosRequestConfig} from "axios";
 import {windowsTool} from "owner-tool-js";
 import {LocalStorageEnum} from "../domain/storage";
 
@@ -6,10 +6,6 @@ export default (config: InternalAxiosRequestConfig)=>{
     if (windowsTool.localStorageTool.get(LocalStorageEnum.token)) {
         // 在请求头中添加token
         config.headers.Authorization = windowsTool.localStorageTool.get(LocalStorageEnum.token);
-        // config.headers = {
-        //     ...config.headers,
-        //     Authorization: windowsTool.localStorageTool.get(LocalStorageEnum.token)
-        // }
     }
     return config;
 }
