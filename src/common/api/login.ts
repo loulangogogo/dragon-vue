47909b31a06f88337540b09d54c9b56617f1b977
loulan <loulangogogo@163.com>
2024-10-18 17:27:26 +0800

    调整授权登陆

--- src/common/api/login.ts
+++ src/common/api/login.ts
@@ -1,6 +1,8 @@
 import ask from "../ask";
 import {LoginData} from "../domain/login";
 import {GrantTypeEnum} from "../domain/enums";
+import {DragonNotice} from "../domain/component";
+import {ResponseResult, ResponseStatusEnum} from "../domain/response";
 
 /**
  * 登录
@@ -10,17 +12,40 @@ import {GrantTypeEnum} from "../domain/enums";
  * */
 export const getToken = (loginData: LoginData) => {
 
-    // let extraParam = `captchaUuid=${loginData.captchaUuid}&captchaCode=${loginData.captchaCode}`;
-    // return ask.post(`${import.meta.env.VITE_REQUEST_AUTH_PRE}/security/token?grant_type=${loginData.grant_type}&client_id=DRAGON_PC&client_secret=123&username=${loginData.account}&password=${loginData.password}&${extraParam}`)
     const urlSearchParams:URLSearchParams = new URLSearchParams({
         grant_type: loginData.grant_type?.toString(),
         client_id: "DRAGON_PC",
         client_secret: "undefined",
-        username: loginData.account?.toString()!,
-        password: loginData.password?.toString()!,
-        captchaUuid: loginData.captchaUuid?.toString()!,
-        captchaCode: loginData.captchaCode?.toString()!,
     });
+
+    // 根据登陆类型设置参数
+    switch (loginData.grant_type) {
+        case GrantTypeEnum.PASSWORD:
+            urlSearchParams.append("username", loginData.account?.toString()!);
+            urlSearchParams.append("password", loginData.password?.toString()!);
+            urlSearchParams.append("captchaUuid", loginData.captchaUuid?.toString()!);
+            urlSearchParams.append("captchaCode", loginData.captchaCode?.toString()!);
+            break;
+        case GrantTypeEnum.WECHAT_SCAN:
+            urlSearchParams.append("code", loginData.account?.toString()!);
+            break;
+        case GrantTypeEnum.PHONE:
+            urlSearchParams.append("username", loginData.account?.toString()!);
+            urlSearchParams.append("password", loginData.password?.toString()!);
+            break;
+        case GrantTypeEnum.EMAIL:
+            urlSearchParams.append("username", loginData.account?.toString()!);
+            urlSearchParams.append("password", loginData.password?.toString()!);
+            break;
+        default:
+            DragonNotice.error("不支持的登陆类型！");
+            return <ResponseResult>{
+                status: ResponseStatusEnum.FAIT,
+                msg: "不支持的登陆类型！"
+            }
+    }
+
+
     return ask.post(`${import.meta.env.VITE_REQUEST_AUTH_PRE}/security/token?${urlSearchParams.toString()}`,{},{
         headers:{
             isNoResponseErrorMsg: GrantTypeEnum.WECHAT_SCAN == loginData.grant_type,
