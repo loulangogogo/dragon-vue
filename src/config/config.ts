e53076dad52bcfd1ab8297541d7128688bbe6bf9
loulan <loulangogogo@163.com>
2024-10-22 14:58:01 +0800

    bug修改调整

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ import './permission';
 // ui组件
 import ArcoVue from '@arco-design/web-vue';
 import ArcoVueIcon from '@arco-design/web-vue/es/icon';
-import '@arco-themes/vue-owner/css/arco.css';
+import '@arco-design/web-vue/dist/arco.css';
 
 // 阿里图标
 import '/public/static/icon/ali/iconfont.css';
